33454034b4e3e999fe29b3e7ca4d8fe10132ce91
Richard Beese <richardbeese@Richards-MacBook-Air.local>
2018-02-07 22:26:07 -0700

    Added new uri request

--- src/services/news.js
+++ src/services/news.js
@@ -8,19 +8,29 @@ import {
 
 export async function getArticles() {
     try {
-        let articles = await fetch(`https://newsapi.org/v2/top-headlines
-        ?api-key=bd08d8d50f834c65b2667d7d733ef50f&language=en`, {
+        return fetch(`https://newsapi.org/v2/top-headlines
+        ?api-key=bd08d8d50f834c65b2667d7d733ef50f&language=en`)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(json) {
+            return orderBy(result.articles, 'publishedAt', 'desc')
+        })
+        //let articles = await fetch(`https://newsapi.org/v2/top-headlines
+        //?api-key=bd08d8d50f834c65b2667d7d733ef50f&language=en`, {
+        
         //let articles = await fetch(`${articles_url}?language=${language}&api-key=${_api_key}`, {
-            header: {
-                'X-API-KEY': _api_key
-            }
-        });
+            //header: {
+              //  'X-API-KEY': _api_key
+            //}
+        //});
         let result = await articles.json();
         articles = null;
 
         return orderBy(result.articles, 'publishedAt', 'desc');
 
     } catch (error) {
+        Alert.alert(JSON.stringify("Something happened, please try again"))
         throw error
     }
 }
